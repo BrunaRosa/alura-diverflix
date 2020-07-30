d22a1c11a576995a8cbc2ad991a96374710c7ab9
Bruna Rosa <brosa@bionexo.com>
2020-07-30 09:21:25 -0300

    Create const AppWrapper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Menu from './components/Menu'
+import styled from 'styled-components' ;
+import Menu from './components/Menu' ;
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
 import Carousel from './components/Carousel';
 import Footer from './components/Footer';
 
+const AppWrapper = styled.div`
+  background: var(--grayDark);
+`;
+
 function App() {
   return (
-    <div style={{ background: "#141414" }}>
+    <AppWrapper style={{ background: "#141414" }}>
       <Menu />
 
       <BannerMain
@@ -42,7 +47,7 @@ function App() {
       />      
 
       <Footer />
-    </div>
+    </AppWrapper>
   );
 }
 
