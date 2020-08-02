fd39d1529550c3e391b7704d4d58665da981a5b9
BrunaRosa <bruninha20_468@hotmail.com>
2020-08-02 04:26:54 -0300

    New lesson

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -3,15 +3,31 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../PageDefault';
 
 function CadastroCategoria() {
-    return (
-      <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-        <Link to="">
-          Ir para a Home
-        </Link>
-      </PageDefault>
-    )
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
+
 
 export default CadastroCategoria;
